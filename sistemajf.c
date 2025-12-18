b78a1199bdc65236cac4638e9d36fcf457e53a15
Isaac <isaacjf321@gmail.com>
2025-12-18 15:46:09 -0300

    Implementação de MENU

---
+++ sistemajf.c
@@ -0,0 +1,98 @@
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+#include <time.h> 
+
+#define tam 50
+
+typedef struct dados_endereco{
+    char cep[tam];
+    char logradouro[tam];
+    int numero; 
+    char complem[tam];
+    char bairro[tam]; 
+    char cidade[tam]; 
+    char uf[5]; 
+}endereco; 
+
+typedef struct dados_pessoais{
+    char nome[tam];
+    int idade; //vai ser transformado em ano,mes e dias. 
+    char cpf[15]; 
+    char telefone[tam];    
+    endereco localizacao; 
+}pessoal; 
+
+typedef struct dados_medicos{
+    char sintomas[150];
+    char nivel; 
+}medico; 
+
+void menu(); 
+
+int main(){
+    setlocale(LC_ALL,"portuguese"); 
+    menu(); 
+}
+
+void menu(){
+    time_t tempo; //para guardar o tempo do computador 
+    struct tm *atual; // uma struct da biblioteca time guarda as datas e horarios atuais , que vao ser traduzidas e guardada pela função, o ponteiro vai servir para pegar os dados 
+    char esc; //escolha de opções no menu 
+    do{
+        while(!kbhit()){ //esquema do relogio e data , e o loop roda enquanto nenhuma tecla for teclada
+            system("cls"); 
+            printf("  _   _            _ _   _     \n");
+            printf(" | | | |          | | | | |    \n");
+            printf(" | |_| | ___  __ _| | |_| |__  \n");
+            printf(" |  _  |/ _ \\/ _` | | __| '_ \\ \n");
+            printf(" | | | |  __/ (_| | | |_| | | |\n");
+            printf(" \\_| |_/\\___|\\__,_|_|\\__|_| |_|\n");
+            printf("                       _ ______\n");
+            printf("                      | |  ____|\n");
+            printf("                   _  | | |__   \n");
+            printf("                  | | | |  __|  \n");
+            printf("                  | |_| | |     \n");
+            printf("                   \\___/|_|     \n");
+            printf("+---------------------------------+\n"); 
+            printf("        SISTEMA HOSPITALAR         \n");
+            printf("+---------------------------------+\n"); 
+            time(&tempo); //pegando o tempo atual do computador e armazenando na variavel 
+            atual=localtime(&tempo); // a função organiza o tempo e coloca na struct , o ponteiro recebe o endereço dos dados organizados na struct, para q possa ser usado
+            printf("HORÁRIO: %02d:%02d:%02d         ",atual->tm_hour,atual->tm_min,atual->tm_sec); //o ponteiro aponta as horas,min,seg da struct traduzida
+            printf("DATA: %d/%d/%d\n",atual->tm_mday,atual->tm_mon+1,atual->tm_year+1900); // o ponteiro indicou dia,mes,ano e foi colocado as correções 
+            puts(""); 
+            puts("Qual opção deseja escolher?"); 
+            printf("    1-Registar dados de paciente\n"); 
+            printf("    2-Registar saúde de paciente\n"); 
+            printf("    3-Realizar atendimento\n"); 
+            printf("    4-Listar pacientes ativos\n"); 
+            printf("    5-Histórico de pacientes\n"); 
+            printf("    6-Sair do sistema\n");  
+            puts(""); 
+            Sleep(1000); //nao sobrecarregar a maquina
+        }  
+        do{ //validação
+            esc=getch(); //pegando a opção 
+        }while(esc!='1' && esc!='2' && esc!='3' && esc!='4' && esc!='5' && esc!='6'); 
+        switch(esc){
+            case '1':
+            break;
+            case '2':
+            break; 
+            case '3':
+            break;
+            case '4':
+            break;
+            case '5':
+            break; 
+            case '6': puts("Saindo do sistema...");
+            break; 
+        }
+    }while(esc!='6');
+
+    return; 
+}
