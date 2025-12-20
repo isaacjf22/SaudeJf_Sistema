0390ee50eae4a3bfba4fbcec1fbdc8171450bd54
Isaac <isaacjf321@gmail.com>
2025-12-20 20:24:54 -0300

    Projeto Finalizado

--- testes.c
+++ testes.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
-#include <windows.h> // Precisa dessa biblioteca!
+#include <windows.h> 
 
 int main() {
-   // PRIMEIRO SOM (O "Dimm")
-    // 750 é a frequência (meio agudo)
-    // 500 é o tempo (meio segundo)
-    Beep(750, 500); 
-    
-    // Pequena pausa dramática
-    Sleep(100); 
-
-    // SEGUNDO SOM (O "Dommm")
-    // 550 é a frequência (mais grave, tom de encerramento)
-    // 800 é o tempo (dura um pouquinho mais)
-    Beep(550, 800);
-    return 0;
+   printf("fim 20/12/2025 20h23min\n");
+   return 0;
 }
     
